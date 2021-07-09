debf790f1c97bcb8d86febcab1117b3a1ac925d6
Zain Kamal <66214025+Humboldt-Penguin@users.noreply.github.com>
2021-07-09 16:48:41 -0400

    Update 3.1.Rutherford_Scattering.py

--- 3.1.Rutherford_Scattering.py
+++ 3.1.Rutherford_Scattering.py
@@ -1,31 +1,74 @@
 from vpython import *
-#GlowScript 3.1 VPython
+
+# GlowScript 3.1 VPython
 
 # scene.autoscale = False
 
 ################################################################################
-# BUTTONS
+# WIDGETS
 
 # Delete the alpha particles (sphere and trail) that have "hit" the borders and are no longer in motion
 def clear_dead_particles(b):
     for p in particles:
-        if (p.dead):
+        if p.dead:
             p.clear_trail()
             p.visible = False
             del p
 
-clear_dead_particles_button = button(bind=clear_dead_particles, text="Clear Dead Particles")
+
+clear_dead_particles_button = button(
+    bind=clear_dead_particles, text="Clear Dead Particles"
+)
 
 
 # Determines how long the "trail" behind each alpha particle is
 def trail_length(s):
     for p in particles:
         p.retain = s.value
-    trail_length_button_caption.text = "Trail = "+"{:1.2f}".format((s.value-1)/10)+"\n\n"
+    trail_length_button_caption.text = (
+        "Trail = " + "{:1.2f}".format((s.value - 1) / 10) + "\n\n"
+    )
 
-trail_length_button = slider(bind=trail_length, min=1, max=1001, value=11, step = 10)
-trail_length_button_caption = wtext(text="Trail = "+"{:1.2f}".format((trail_length_button.value-1)/10)+"\n\n")
 
+trail_length_button = slider(bind=trail_length, min=1, max=1001, value=11, step=10)
+trail_length_button_caption = wtext(
+    text="Trail = " + "{:1.2f}".format((trail_length_button.value - 1) / 10) + "\n\n"
+)
+
+
+running = True
+
+
+def Run(b):  # b = button
+    global running, remember_dt, dt
+    running = not running
+    if running:
+        b.text = "Pause Movement"
+        dt = remember_dt
+    else:
+        b.text = "Resume Movement"
+        remember_dt = dt
+        dt = 0
+    return
+
+
+button(text="Pause Movement", bind=Run)
+
+
+running_ = True
+
+
+def Spawn(bu):  # bu = button
+    global running_
+    running_ = not running_
+    if running_:
+        bu.text = "Pause Spawning"
+    else:
+        bu.text = "Resume Spawning"
+    return
+
+
+button(text="Pause Spawning", bind=Spawn)
 
 
 ################################################################################
@@ -33,15 +76,14 @@ trail_length_button_caption = wtext(text="Trail = "+"{:1.2f}".format((trail_leng
 
 # Reference/GravityFunction modified such that the force is repulsive, as opposed to attractive
 def f_e(p1, p2):
-    if (p1.pos == p2.pos):
-        return vec(0,0,0)
+    if p1.pos == p2.pos:
+        return vec(0, 0, 0)
     k = 2.5
     r_vec = p2.pos - p1.pos
-    force_mag = k * p1.charge * p2.charge / mag(r_vec)**2
-    r_hat = r_vec/mag(r_vec)
+    force_mag = k * p1.charge * p2.charge / mag(r_vec) ** 2
+    r_hat = r_vec / mag(r_vec)
     force_vec = -force_mag * r_hat
     return force_vec
-
 
 
 ################################################################################
@@ -57,58 +99,51 @@ alpha_rate = 1000
 dt = 0.001
 
 
-
-
-
 nucleus = simple_sphere(
-    pos = vec(xrange/2, 0, 0),
-    radius = 0.5,
-    color = color.red, #vec(0.867, 0.467, 0.467),
-    charge = 1
+    pos=vec(xrange / 2, 0, 0),
+    radius=0.5,
+    color=color.red,  # vec(0.867, 0.467, 0.467),
+    charge=1,
 )
 
 particles = []
 
 i = 0
 
-for time in range(0,1000,dt):
-    rate(5/dt)
-    
-    
+for time in range(0, 1000, dt):
+    rate(5 / dt)
+
     i += 1
-    
-    if (i % alpha_rate == 0):
-        
+
+    if running_ and i % alpha_rate == 0:
+
         i = 0
 
         particles.append(
             simple_sphere(
-                pos = vec(-xrange/2, -yrange/2 + random()*yrange, 0),
-                radius = 0.3,
-                color = vec(1,50,40),
-                momentum = vec(2,0,0),
-                mass = 1,
-                charge = 1,
-                make_trail = True,
-                retain = trail_length_button.value,
-                dead = False
+                pos=vec(-xrange / 2, -yrange / 2 + random() * yrange, 0),
+                radius=0.3,
+                color=vec(1, 50, 40),
+                momentum=vec(2, 0, 0),
+                mass=1,
+                charge=1,
+                make_trail=True,
+                retain=trail_length_button.value,
+                dead=False,
             )
         )
-        
-
 
     for p in particles:
-        if (not p.dead):
-            p.force = f_e(p,nucleus)
+        if not p.dead:
+            p.force = f_e(p, nucleus)
             p.momentum = p.momentum + p.force * dt
             p.pos = p.pos + (p.momentum / p.mass) * dt
-        
-        if (abs(p.pos.y) > yrange or abs(p.pos.x) > xrange):
+
+        if abs(p.pos.y) > yrange or abs(p.pos.x) > xrange:
             # particles.remove(p)
-            p.momentum = vec(0,0,0)
+            p.momentum = vec(0, 0, 0)
             p.dead = True
-    
-            
-            
+
     time += dt
+    
     
